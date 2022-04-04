ebb3cf653d5022b8be63c3f2232b0eec7149c2b3
Moonshield7 <fannie.p@hotmail.fr>
2022-04-04 17:08:52 +0200

    Add register action + make authentication

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class,[
+                'label' => 'Email :',
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe :',
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom :',
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom :',
+            ])
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Civilité :',
+                'expanded' => true,
+                'choices' => [
+                    'Homme' => 'h',
+                    'Femme' => 'f',
+                    'Autre' => 'x'
+                ],
+                'attr' => [
+                    'class' => '',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label'=> 'Valider',
+                // Cette option permet de désactiver le validator HTML (front)
+                    # => form_start(form, {'attr': {'novalidate': novalidate}})
+                'validate' => false,
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
